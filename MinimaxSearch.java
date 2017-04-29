b531dd095686a3e60b4624bdd21dfa997d7225f9
Rahul Jevisetty <rahul.jevisetty@uconn.edu>
2017-04-29 15:29:58 -0400

    Add files via upload

--- MinimaxSearch.java
+++ MinimaxSearch.java
@@ -3,23 +3,23 @@ import java.util.List;
 public class MinimaxSearch {
 	
 	private String myPlayer;
-	private static final int FIXED_DEPTH = 8;
+	private static final int FIXED_DEPTH = 11;
 	
 	public MinimaxSearch(String mp) {
 		myPlayer = mp;
 	}
 
-	public Move minimaxDecision(GameState state) {
+	public Move minimaxDecision(GameState state, double alpha, double beta) {
 		List<Move> actions = state.actions();
 		if (actions.size() == 0) return null;
 		
 		Move bestMove = null;
 		boolean maximizingFirst = state.player().equals(myPlayer);
-		double bestScore = maximizingFirst ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+		double bestScore = maximizingFirst ? alpha : beta;
 		int currentDepth = 1;
 		for (Move move : actions) {
 			GameState successor = state.result(move);
-			double score = maximizingFirst ? minValue(successor, currentDepth) : maxValue(successor, currentDepth);
+			double score = maximizingFirst ? minValue(successor, currentDepth, alpha, beta) : maxValue(successor, currentDepth, alpha, beta);
 			if ((maximizingFirst && score >= bestScore) || (!maximizingFirst && score <= bestScore)) {
 				bestScore = score;
 				bestMove = move;
@@ -39,6 +39,30 @@ public class MinimaxSearch {
 			if (score >= bestScore) bestScore = score;
 		}
 		return bestScore;
+	}
+	
+	private double maxValue(GameState state, int currentDepth, double alpha, double beta) {
+		if (state.isTerminal() || currentDepth == FIXED_DEPTH) return state.utility(myPlayer);
+		//double bestScore = Integer.MIN_VALUE;
+		for (Move move : state.actions()) {
+			GameState successor = state.result(move);
+			double score = minValue(successor, currentDepth + 1, alpha, beta);
+			if (score > alpha) alpha = score;
+			if (alpha >= beta) break;
+		}
+		return alpha;
+	}
+	
+	private double minValue(GameState state, int currentDepth, double alpha, double beta) {
+		if (state.isTerminal() || currentDepth == FIXED_DEPTH) return state.utility(myPlayer);
+		//double bestScore = Integer.MAX_VALUE;
+		for (Move move : state.actions()) {
+			GameState successor = state.result(move);
+			double score = maxValue(successor, currentDepth + 1, alpha, beta);
+			if (score < beta) beta = score;
+			if (alpha >= beta) break;
+		}
+		return beta;
 	}
 	
 	private double minValue(GameState state, int currentDepth) {
